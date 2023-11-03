3ece7b836ad5072d129b04022f5c2ae227112f63
joshchopin7 <126109111+joshchopin7@users.noreply.github.com>
2023-11-02 19:07:19 -0700

    class prototypes

--- candidatetype.h
+++ candidatetype.h
@@ -6,12 +6,27 @@
 
 const int NUM_OF_CAMPUSUS = 4;
 
-class CandidateType:PersonType
+
+class CandidateType:public PersonType
 {
+public:
+    CandidateType();
 
+    int getTotalVotes() const;
+    int getVotesByCampus(int campusArr[]) const;
+    
+    void updateVotesByCampus(int totalVotes, int votesForCampus);
+    void printCandidateInfo();
+    void printCandidateInfo();
+    void printCandidateCampusVotes();
 
+    ~CandidateType();
 
+private:
+    int numVotes;
+    int campusArr[NUM_OF_CAMPUSES];
 };
+
 
 
 
